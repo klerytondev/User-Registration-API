e90596731bcbaa8915c0d14af1b88ebb24ffa3e1
Kleryton Souza <kleryton.dev@gmail.com>
2023-02-12 16:40:27 -0300

    Project Update

--- src/main/java/br/com/kleryton/api/resoucers/UserResource.java
+++ src/main/java/br/com/kleryton/api/resoucers/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+
     @Autowired
     private UserServiceImpl userService;
 
