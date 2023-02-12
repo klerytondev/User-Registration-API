8e7a771ddb6ae36dfb7a0f2f6ba9fea772b12f06
Kleryton Souza <kleryton.dev@gmail.com>
2023-02-12 14:35:22 -0300

    Created User Class

---
+++ src/main/java/br/com/kleryton/api/domain/User.java
@@ -0,0 +1,20 @@
+package br.com.kleryton.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
+
