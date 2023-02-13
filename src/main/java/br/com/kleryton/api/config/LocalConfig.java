f7382bc89f2c8caff6cb245714a457b7b62f61da
Kleryton Souza <kleryton.dev@gmail.com>
2023-02-13 20:42:05 -0300

    Implemented Config Class

--- src/main/java/br/com/kleryton/api/config/LocalConfig.java
+++ src/main/java/br/com/kleryton/api/config/LocalConfig.java
@@ -1,10 +1,13 @@
 package br.com.kleryton.api.config;
 
+import br.com.kleryton.api.domain.User;
 import br.com.kleryton.api.repositories.UserRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
 @Profile("local")
@@ -14,7 +17,9 @@ public class LocalConfig {
     private UserRepositorie userRepositorie;
 
     @Bean
-    public void startDB(){
-
+    public void startDB() {
+        User user01 = new User(null, "Kleryton", "kleryton.inside@gmail.com", "123");
+        User user02 = new User(null, "Maira", "maira.luana20@gmail.com", "456");
+        userRepositorie.saveAll(List.of(user01, user02));
     }
 }
