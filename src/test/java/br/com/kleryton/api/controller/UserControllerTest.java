57cb6ef0aa3b483339827a98a5e045ed2062226e
Kleryton Souza <kleryton.dev@gmail.com>
2023-02-17 17:53:02 -0300

    Implemented method whenFindByIDThenReturnSuccess class UserControllerTest

--- src/test/java/br/com/kleryton/api/controller/UserControllerTest.java
+++ src/test/java/br/com/kleryton/api/controller/UserControllerTest.java
@@ -10,6 +10,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserControllerTest {
@@ -43,7 +50,22 @@ class UserControllerTest {
     }
 
     @Test
-    void findByID() {
+    void whenFindByIDThenReturnSuccess() {
+        when(userService.findByID(anyLong())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDto);
+
+        ResponseEntity<UserDTO> response = controller.findByID(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
+
+
     }
 
     @Test
