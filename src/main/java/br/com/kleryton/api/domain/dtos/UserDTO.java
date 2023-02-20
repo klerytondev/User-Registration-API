2273d9a0cf6d8ca0ae4e2b02fb7a8ab66a792f03
Kleryton Souza <kleryton.dev@gmail.com>
2023-02-20 13:18:38 -0300

    Included file to deploy Heroku

--- src/main/java/br/com/kleryton/api/domain/dtos/UserDTO.java
+++ src/main/java/br/com/kleryton/api/domain/dtos/UserDTO.java
@@ -3,11 +3,13 @@ package br.com.kleryton.api.domain.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Long id;
